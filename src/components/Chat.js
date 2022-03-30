19976e7e4f749379080662b1ac141407e0cfa187
luke-meadows <lukemeadowsdev@gmail.com>
2022-03-30 14:36:47 +0100

    conversation

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -5,25 +5,61 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import { v4 as uuidv4 } from 'uuid';
 import Conversation from './Conversation';
+import AddIcon from '@mui/icons-material/Add';
+import { db } from '../db/firebase';
 
 export const Chat = React.memo(() => {
   const user = useSelector(selectUser);
   const [conversations, setConversations] = useState([]);
+  const [activeChat, setActiveChat] = useState(null);
+  const [selectUserToChatOpen, setSelectUserToChatOpen] = useState(false);
+  const [allUsers, setAllUsers] = useState([]);
 
   useEffect(async () => {
     const userConversations = await getChats(user.uid);
     setConversations(userConversations);
   }, []);
 
+  async function getAllUsers() {
+    const users = await db
+      .collection('users')
+      .where('userId', '!=', user.uid)
+      .get();
+    const userData = users.docs.map((doc) => doc.data());
+    setAllUsers(userData);
+  }
+
   return (
     <div className="chat">
       <div className="chat__top">
         <h3>Chat</h3>
+        <AddIcon
+          onClick={async () => {
+            getAllUsers();
+            setSelectUserToChatOpen(!selectUserToChatOpen);
+          }}
+        />
       </div>
       {conversations.map((convo) => {
         const key = uuidv4();
-        return <Conversation key={key} conversation={convo} />;
+        return (
+          <p key={key} onClick={() => setActiveChat(convo)}>
+            {convo.participant}
+          </p>
+        );
       })}
+
+      {activeChat && (
+        <Conversation conversation={activeChat} setActiveChat={setActiveChat} />
+      )}
+
+      {selectUserToChatOpen && (
+        <div className="select__user__to__chat">
+          {allUsers?.map((user) => (
+            <p key={user.userId}>{user.username}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
 });
--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from 'react';
 import { db } from '../db/firebase';
 import { v4 as uuidv4 } from 'uuid';
 import CloseIcon from '@mui/icons-material/Close';
-
+import firebase from 'firebase';
 import '../styles/Chat.css';
+import { selectUser } from '../features/userSlice';
+import { useSelector } from 'react-redux';
 
-export default function Conversation({ conversation }) {
+export default function Conversation({ conversation, setActiveChat }) {
+  const user = useSelector(selectUser);
   const [messageText, setMessageText] = useState('');
   const [messages, setMessages] = useState([]);
-  const [conversationOpen, setConversationOpen] = useState(false);
-  const [conversationWindowOpen, setConversationWindowOpen] = useState(false);
-  const [activeConversation, setActiveConversation] = useState({});
-  useEffect(async () => {
+  useEffect(() => {
     // Collect the messages from the chat Id
     db.collection('chats')
       .doc(conversation.id)
@@ -25,62 +25,57 @@ export default function Conversation({ conversation }) {
         });
         setMessages(returnedMessages);
       });
-    // return () => setFullscreen(false);
   }, []);
 
-  useEffect(() => {
-    if (!conversationOpen) return;
-    setActiveConversation({
-      participant: conversation.participant,
-      messages: messages,
-    });
-  }, [messages]);
-
-  function openConversation() {
-    console.log('click');
-    setConversationOpen(true);
-    setActiveConversation({
-      participant: conversation.participant,
-      messages: messages,
-    });
-    setConversationWindowOpen(true);
+  function sendMessage(e) {
+    e.preventDefault();
+    db.collection('chats')
+      .doc(conversation.id)
+      .collection('messages')
+      .doc()
+      .set({
+        message: messageText,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        owner: user.uid,
+      });
+    setMessageText('');
   }
 
   return (
     <div>
-      <p className="conversation__button" onClick={openConversation}>
-        {conversation.participant}
-      </p>
-      {conversationWindowOpen && (
-        <div className="conversation">
-          <div className="conversation__top">
-            <h6>{activeConversation.participant}</h6>
-            <CloseIcon onClick={() => setConversationWindowOpen(false)} />
-          </div>
-          <div className="conversation__messages">
-            {activeConversation.messages.map((message, i) => {
-              const key = uuidv4();
-              return (
-                <p
-                  key={key}
-                  className={i % 2 > 0 ? 'message sent' : 'message received'}
-                >
-                  {message.message}
-                </p>
-              );
-            })}
-          </div>
-          <form action="">
-            <input
-              placeholder="Say something"
-              type="text"
-              value={messageText}
-              onChange={(e) => setMessageText(e.target.value)}
-            />
-            <button>Send</button>
-          </form>
+      <div className="conversation">
+        <div className="conversation__top">
+          <h6>{conversation.participant}</h6>
+          <CloseIcon onClick={() => setActiveChat(null)} />
         </div>
-      )}
+        <div className="conversation__messages">
+          {messages.map((message) => {
+            const key = uuidv4();
+            return (
+              <p
+                key={key}
+                className={
+                  message.userId === user.uid
+                    ? 'message sent'
+                    : 'message received'
+                }
+              >
+                {message.message}
+              </p>
+            );
+          })}
+        </div>
+        <form action="" onSubmit={sendMessage}>
+          <input
+            placeholder="Say something"
+            type="text"
+            value={messageText}
+            onChange={(e) => setMessageText(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+      {/* )} */}
     </div>
   );
 }
